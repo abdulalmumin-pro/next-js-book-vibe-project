a00d898c5bb81dfff73de69dff72bcd4483c1dcd
abdulalmumin-pro <abdulalmumin0332@gmail.com>
2026-09-22 20:47:05 +0600

    fixed type error & whole project updated

--- src/context/BooksContext.tsx
+++ src/context/BooksContext.tsx
@@ -1,12 +1,24 @@
 "use client";
+import { Ibook } from '@/types/bookType';
 import { createContext, ReactNode, useState } from 'react';
+interface IbookContext {
+    readBook: Ibook[],
+    setReadBook: React.Dispatch<React.SetStateAction<Ibook[]>>,
+    wishList: Ibook[],
+    setWishList:React.Dispatch<React.SetStateAction<Ibook[]>>
+}
 
-export const booksContext = createContext({})
+export const booksContext = createContext<IbookContext>({
+    readBook: [],
+    setReadBook: () => {},
+    wishList: [],
+    setWishList: () => {},
+});
 
 const BooksProvider = ( {children}: {children: ReactNode} ) => {
 
-    const [readBook, setReadBook] = useState([])
-    const [wishList, setWishList] = useState([])
+    const [readBook, setReadBook] = useState<Ibook[]>([])
+    const [wishList, setWishList] = useState<Ibook[]>([])
 
     const shareData = {
         readBook,
